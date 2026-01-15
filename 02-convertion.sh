d28f2046910868b8edfdc7fd06da184e045c3881
jamithiprudhvi <jamithireddyprudhvi0906@gmail.com>
2026-01-16 04:07:38 +0530

    variables

--- 02-convertion.sh
+++ 02-convertion.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "hanuman: hi rama parbhu"
+echo "hanuman: hi Rama parbhu"
 echo "Rama: hi hanuman"
 echo "hanuman: how are you?"
 echo "Rama: I am fine"
---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+person1="Rama"
+person2="hanuman"
+
+
+echo "$person2: hi $person1 parbhu"
+echo "$person1: hi $person2"
+echo "$person2: how are you?"
+echo "$person1: I am fine"
