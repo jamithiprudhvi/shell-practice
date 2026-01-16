8e9d2facaed9b80b5b791da507603c1512f18bee
jamithiprudhvi <jamithireddyprudhvi0906@gmail.com>
2026-01-16 10:45:05 +0530

    array

--- 07-array.sh
+++ 07-array.sh
@@ -3,4 +3,4 @@ TIMESTAMP=$(date)
 echo "The script is running on $TIMESTAMP"
 
 MOVIES=("MSVP", "RRR", "KGF2", "Vikram", "Pushpa")
-echo "My favorite movie is ${MOVIES[2]}"
+echo "My favorite movie is ${MOVIES[@]}"
